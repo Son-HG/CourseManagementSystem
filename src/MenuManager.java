2595a57584d264196b9d1e4239696f2d1fc6963a
Son-HG <ha4896@naver.com>
2021-03-11 21:04:37 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Subject");
+		System.out.println("2. Delete Subject");
+		System.out.println("3. Edit Timetable");
+		System.out.println("4. View Timetable");
+		System.out.println("5. Exit");
+	}
+
+}
