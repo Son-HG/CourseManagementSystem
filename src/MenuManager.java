a79042c649a9075cc12738a9011495520c949984
Son-HG <ha4896@naver.com>
2021-04-06 16:13:40 +0900

    Now you can add, delete, edit, and view courses.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,31 +3,30 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
 		Scanner input = new Scanner(System.in);
-		
+		CourseManager courseManager = new CourseManager(input);
+		int num = 0;
 	
-		while (num != 6) {
+		while (num != 5) {
 			System.out.println("*** Course Management System Menu ***");
 			System.out.println("1. Add Course");
 			System.out.println("2. Delete Course");
 			System.out.println("3. Edit Course");
 			System.out.println("4. View Course");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6:");
+			System.out.println("5. Exit");
+			System.out.println("Select one number between 1-5:");
 			num = input.nextInt();
 			if (num == 1) {
-				AddCourse();	
+				courseManager.addCourse();	
 			}
 			else if (num == 2) {
-				DeleteCourse();
+				courseManager.deleteCourse();
 			}
 			else if (num == 3) {
-				EditCourse();
+				courseManager.editCourse();
 			}
 			else if (num == 4) {
-				ViewCourse();
+				courseManager.viewCourse();
 			}
 			else {
 				continue;
@@ -37,36 +36,4 @@ public class MenuManager {
 		
 	}
 	
-	public static void AddCourse() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Course number:");
-		String courseNumber = input.next();
-		System.out.print("Class number:");
-		int classNumber = input.nextInt();
-		System.out.print("Course name:");
-		String courseName = input.next();
-		System.out.print("Professor name:");
-		String professorName = input.next();
-		System.out.print("Credit:");
-		int credit = input.nextInt();
-	}
-	
-	public static void DeleteCourse() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Course number:");
-		String courseNumber = input.next();
-	}
-	
-	public static void EditCourse() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Course number:");
-		String courseNumber = input.next();
-	}
-	
-	public static void ViewCourse() {
-		Scanner input = new Scanner(System.in); 
-		System.out.print("Course number:");
-		String courseNumber = input.next();
-	}
-
 }
